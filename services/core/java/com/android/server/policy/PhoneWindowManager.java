8c29681731e56cf5a088ec1ff83dd7469713842a
Felipe Leon <fglfgl27@gmail.com>
2017-10-17 17:13:37 -0200

    Base: prevent haptic feedback if screen is not fully on
    Don't know if this affects all devices, problem identified on a device with hw keys that are cover by the same glass as the main screen,
    if DT2W and hapitc feedback is enable the hw keys will cause vibration when the screen is off
    prevent that by checking if the screen is fully on...
    DT2W active area doesnot cover the hw key area has the hw keys can't be used to wakeup the device via DT2W
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4229,7 +4229,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         boolean useHapticFeedback = down
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
                 && (!isNavBarVirtKey || mNavBarVirtualKeyHapticFeedbackEnabled)
-                && event.getRepeatCount() == 0;
+                && event.getRepeatCount() == 0
+                && mDefaultDisplayPolicy.isScreenOnFully();
 
         // Specific device key handling
         if (dispatchKeyToKeyHandlers(event)) {
